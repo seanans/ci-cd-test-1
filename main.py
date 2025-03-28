3c63b0fe3da4431e00bc10888a993f4bee53bef2
seanans <maks1998ivan@gmail.com>
2025-03-28 12:27:56 +0200

    method

---
+++ main.py
@@ -0,0 +1,8 @@
+def filter_lines(filename, keyword):
+    with open(filename, 'r') as infile:
+        lines = infile.readlines()
+    filtered_lines = [line for line in lines if keyword in line]
+    with open('filtered.txt', 'w') as outfile:
+        outfile.writelines(filtered_lines)
+
+filter_lines('text.txt', 'incididunt')
